ff0392fe9006dcb4990c76b8471db20ae77c0745
Márcio-Brust <brustmw21@gmail.com>
2023-12-20 09:47:20 -0300

    projetoNike

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        showTenis: "showTenis 0.3s ease-in-out forwards",
+      },
+      keyframes: {
+        showTenis: {
+          from: { transform: "rotate(0deg)", width: "400px" },
+          to: {
+            transform: "rotate(8deg)",
+            position: "absolute",
+            width: "500px",
+          },
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
